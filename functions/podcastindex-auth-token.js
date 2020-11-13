6d76db8991ef4528101d884d8515fd19b9e4ec74
Templin, Chris <ctemplin@gmail.com>
2020-11-13 13:47:22 -0500

    Rounding millisecs to full seconds. Fixing var case

--- functions/podcastindex-auth-token.js
+++ functions/podcastindex-auth-token.js
@@ -1,16 +1,16 @@
 let sha1 = require('sha1');
 
 exports.handler = async function(event, context) {
-    const unixtime = Date.now()
+    const unixtime = Math.floor(Date.now() / 1000)
     const key = process.env.PODCASTINDEX_API_KEY
     const secret = process.env.PODCASTINDEX_API_SECRET
-    const hash = sha1(key + secret + unixtime)
+    const hash = sha1(key + secret + unixtime.toString())
 
     return {
         statusCode: 200,
         body: JSON.stringify(
             {
-                "X-Auth-date": unixtime,
+                "X-Auth-Date": unixtime,
                 "X-Auth-Key": key,
                 "Authorization": hash
             }
