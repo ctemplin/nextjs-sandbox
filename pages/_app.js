d83f5e632ab8fe6f904011701a03c72f7b0ef4b4
Templin, Chris <ctemplin@gmail.com>
2020-12-05 22:09:49 -0500

    Title change

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={cache}>
       <Head>
-        <title>My page</title>
+        <title>Podcast Index Explorer</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
