ee60e4e0fd738112411e8dd8fa708ccbb6e5eb24
Templin, Chris <ctemplin@gmail.com>
2020-12-05 22:26:07 -0500

    Adding page and lambda for new feeds page

---
+++ functions/podcastindex-newfeeds.js
@@ -0,0 +1,36 @@
+let sha1 = require('sha1');
+let fetch = require('node-fetch')
+
+exports.handler = async function(event, context) {
+
+    // TODO: move to shared location
+    const unixtime = Math.floor(Date.now() / 1000);
+    const key = process.env.PODCASTINDEX_API_KEY;
+    const secret = process.env.PODCASTINDEX_API_SECRET;
+    const hash = sha1(key + secret + unixtime.toString());
+    var authHeaders = {
+        "X-Auth-Date": unixtime,
+        "X-Auth-Key": key,
+        "Authorization": hash
+    }
+
+    var json
+    try {
+        const pdiReq  = await fetch(
+            "https://api.podcastindex.org/api/1.0/recent/newfeeds?max=20&lang=en",
+            {
+                method: 'GET',
+                headers: authHeaders
+            }
+        );
+        json = await pdiReq.json()
+    } catch (error) {
+        console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(json.feeds)
+    };
+}
---
+++ pages/new.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import material, {
+    Container,
+    Grid,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Box,
+    Button,
+    Paper,
+    Icon,
+    SvgIcon
+} from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import Navigation from "../src/Navigation";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
+import useSWR from "swr"
+import dayjs  from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+dayjs.extend(relativeTime)
+
+export async function getStaticProps() {
+    return {
+        props: {}
+    }
+}
+
+function renderLinkCell(params) {
+    return <a href={params.getValue("url")} target="_blank">{params.value}</a>;
+}
+
+function formatDay(date) {
+    return dayjs().to(dayjs(date))
+}
+
+function renderUpdateCell(params) {
+    return <span>{formatDay(new Date(params.value * 1000).toLocaleString())}</span>
+}
+
+export default function About({ }) {
+    const { data, error } = useSWR(
+        "/.netlify/functions/podcastindex-newfeeds",
+        (...args) => fetch(...args).then(res => res.json(),
+                                         {dedupingInterval: 15000, revalidateOnFocus: false}
+        )
+    )
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    const dataCols = [
+    { field: "url",
+      headerName: "Title",
+      width: 400,
+      renderCell: renderLinkCell
+    },
+    { field: "timeAdded",
+      headerName: "Time Added",
+      width: 200,
+      renderCell: renderUpdateCell
+    },
+    ];
+
+    return ( 
+    <Container maxWidth="lg">
+        <Grid container spacing="3" direction="row-reverse">
+            <Grid item xs={12}>
+                <Paper elevation={3}>
+                    <Typography variant="h4">Podcast Index Explorer</Typography>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper elevation={3}>
+                    <Navigation></Navigation>
+                </Paper>
+            </Grid>
+            <Grid item xs={9} height="200">
+                <Typography variant="h5">Newly Added Feeds</Typography>
+                <DataGrid
+                    rows={data}
+                    columns={dataCols}
+                    pageSize={10}
+                    autoHeight="false"
+                    disableExtendRowFullWidth="true"
+                    checkboxSelection
+                />
+            </Grid>
+        </Grid>
+    </Container>
+    );
+    }
