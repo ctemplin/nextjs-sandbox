31d38d5525760d06a5ee9942c0b1da680e37336a
Templin, Chris <ctemplin@gmail.com>
2021-02-01 11:25:56 -0500

    Adding configs for tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/index.js'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
